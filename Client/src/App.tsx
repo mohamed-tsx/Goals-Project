9e9ec63e468c2e94f4d43367c6e292555abb1b1f
Mohamed Mohamud <mohammedmohamuud.01@gmail.com>
2023-11-21 18:10:36 -0800

    Added frontend UI for Login and Register

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,9 +1,23 @@
-function App() {
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./Pages/Dashboard";
+import Register from "./Pages/Register";
+import Login from "./Pages/Login";
+import Header from "./Components/Header";
+const App = () => {
   return (
-    <div>
-      <h1>My App</h1>
-    </div>
+    <>
+      <Router>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
-}
+};
 
 export default App;
